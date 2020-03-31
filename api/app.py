95253371bbbc47c5fb43e766940b7abc2bb3c6c3
cail <cail1844@gmail.com>
2020-03-31 23:45:23 +0800

    完成字幕查询和下载接口

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from thunder_subtitle.search import search
+from flask import Flask, request
+from thunder_subtitle.search import search, get_url
 
 
 app = Flask(__name__)
@@ -8,10 +8,26 @@ app = Flask(__name__)
 def api_state():
   return {'state': 'ok'}, 200
 
-@app.route('/api/get-subtitle')
-def get_subtitle():
+@app.route('/api/subs', methods=['POST'])
+def get_subs():
+  print('fpath: ', request.form.get('fpath'))
+  fpath = request.form.get('fpath')
+  if not fpath:
+    fpath = request.args.get('fpath')
+  subs = search(fpath)
+  if not subs:
+    return {'message': '超过最大重试次数后仍然未能获得正确结果', 'subs':[]}, 404
+  subs.sort(key=lambda x: x['rate'], reverse=True)
+  return {'message': 'search success', 'subs': subs}, 200
+
+@app.route('/api/downsub', methods=['POST'])
+def download_sub():
+  surl = request.form.get('surl')
+  sname = request.form.get('sname')
+  data = get_url(surl)
+  with open(sname, 'wb') as f:
+    f.write(data)
   return {'message': 'download success'}, 200
 
-
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
