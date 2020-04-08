6d4b8bf73a2036b3ea8ed7a3b7a9f2bd813298cc
cail <cail1844@gmail.com>
2020-04-08 23:37:39 +0800

    静态版本

--- src/App.js
+++ src/App.js
@@ -16,10 +16,87 @@ function App() {
         </p>
 
       </header>
-          <DraggerItem />
+          <DraggerItem files={Files} />
           <button type="primary">Button</button>
     </div>
   );
 }
+
+const Files = [
+  {name: '寄生虫.Parasite.2019.KORAEN.1080p.BluRay.x264.mkv', path: '/home/cail/d/迅雷下载\寄生虫.Parasite.2019.KORAEN.1080p.BluRay.x264.mkv', size: 1111111},
+]
+
+const Subs = [
+  [
+    {
+      "message": "search success",
+      "subs": [
+        {
+          "language": "未知语言",
+          "rate": "3",
+          "roffset": 6439746936,
+          "scid": "DC0892FE42C95635562369A8B194D0B01FF763D4",
+          "sname": "Parasite.2019.WEB-DLRip.2.18Gb.MegaPeer.srt",
+          "surl": "http://subtitle.v.geilijiasu.com/DC/08/DC0892FE42C95635562369A8B194D0B01FF763D4.srt",
+          "svote": 948
+        },
+        {
+          "language": "未知语言",
+          "rate": "1",
+          "roffset": 6439746936,
+          "scid": "DA5219F551B7F823F9C4B752CF59A3C7B771B959",
+          "sname": "Parasite.2019.1080p.HDRip.X264.AC3-EVO.srt",
+          "surl": "http://subtitle.v.geilijiasu.com/DA/52/DA5219F551B7F823F9C4B752CF59A3C7B771B959.srt",
+          "svote": 7
+        },
+        {
+          "language": "未知语言",
+          "rate": "0",
+          "roffset": 6439746936,
+          "scid": "A30068F3164BDE0F8BC9CA75FACE889CF6F2DA92",
+          "sname": "C%3A%5C%EC%98%81%ED%99%94%5C%E7%9B%97%E6%A2%A6%E7%A9%BA%E9%97%B4.Inception.2010.BD1080P.%E8%8B%B1%E5%9B%BD%E5%8F%8C%E8%AF%AD%E4%B8%AD%E8%8B%B1%E5%8F%8C%E5%AD%97.BTDX8%5CInception.2010_kor.smi",
+          "surl": "http://subtitle.v.geilijiasu.com/A3/00/A30068F3164BDE0F8BC9CA75FACE889CF6F2DA92.smi",
+          "svote": 2
+        },
+        {
+          "language": "未知语言",
+          "rate": "0",
+          "roffset": 6439746936,
+          "scid": "77AC34BF24C1E50BDDD7302643FCF69A646616B3",
+          "sname": "Parasite.2019.KOREAN.1080p.BluRay.x264.DTS-HD.MA.7.1-FGT.srt",
+          "surl": "http://subtitle.v.geilijiasu.com/77/AC/77AC34BF24C1E50BDDD7302643FCF69A646616B3.srt",
+          "svote": 7
+        },
+        {
+          "language": "�?体",
+          "rate": "0",
+          "roffset": 6439746936,
+          "scid": "BF6AD0B41866DDEC4072BC96966439F98B04E58E",
+          "sname": "寄生兽.srt",
+          "surl": "http://subtitle.v.geilijiasu.com/BF/6A/BF6AD0B41866DDEC4072BC96966439F98B04E58E.srt",
+          "svote": 7
+        },
+        {
+          "language": "未知语言",
+          "rate": "0",
+          "roffset": 6439746936,
+          "scid": "9DB8FFABDFFF715F3D8553D355163FFDF8E34A83",
+          "sname": "F%3A%5C%5B%E9%9F%A9%E5%9B%BD%5D%20%E4%B8%AD%E5%9B%BD%E5%9F%8E%20%5B%E9%9F%A9%E8%AF%AD%E4%B8%AD%E5%AD%97%5DCoin.Locker.Girl.2015.KOREAN.1080p.WEBRip.x264-VXT%5B2.1GB%5Dmp4%5CCoin.Locker.Girl.2015.KOREAN.1080p.WEBRip.x264-VXT.chs.srt",
+          "surl": "http://subtitle.v.geilijiasu.com/9D/B8/9DB8FFABDFFF715F3D8553D355163FFDF8E34A83.srt",
+          "svote": 5
+        },
+        {
+          "language": "未知语言",
+          "rate": "0",
+          "roffset": 6439746936,
+          "scid": "CFE5726755878A86C4652525FB76648622DDEA5E",
+          "sname": "Parasite.2019.WEB.srt",
+          "surl": "http://subtitle.v.geilijiasu.com/CF/E5/CFE5726755878A86C4652525FB76648622DDEA5E.srt",
+          "svote": 4
+        }
+      ]
+    }
+  ],
+]
 
 export default App;
--- src/components/DraggerItem.js
+++ src/components/DraggerItem.js
@@ -1,75 +1,42 @@
-import React, {useMemo} from 'react';
-import {useDropzone} from 'react-dropzone';
+import React, {Component} from 'react';
+import Dropzone from 'react-dropzone';
 
-const baseStyle = {
-  flex: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '20px',
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  outline: 'none',
-  transition: 'border .24s ease-in-out'
-};
+class Basic extends Component {
+  // constructor() {
+  //   super();
+  //   this.onDrop = (files) => {
+  //     this.setState({files});
+  //     console.log(files);
+  //   };
+  //   this.state = {
+  //     files: []
+  //   };
+  // }
 
-const activeStyle = {
-  borderColor: '#2196f3'
-};
+  render() {
+    const files = this.props.files.map(file => (
+      <li key={file.name}>
+        {file.name} - {file.size} bytes
+      </li>
+    ));
 
-const acceptStyle = {
-  borderColor: '#00e676'
-};
-
-const rejectStyle = {
-  borderColor: '#ff1744'
-};
-
-function StyledDropzone(props) {
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
-    acceptedFiles,
-  } = useDropzone({accept: 'image/*'});
-
-  // const files = acceptedFiles.map(file => <li key={file.path}>{file.path}</li>);
-
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isDragActive ? activeStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {})
-  }), [
-    isDragActive,
-    isDragAccept,
-    isDragReject
-  ]);
-
-  return (
-    <section className="container">
-      <div {...getRootProps({style})}>
-        <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-      <aside>
-          <h4>Files</h4>
-          <ul>
-            {acceptedFiles.map(file => (
-              <li key={file.path}>
-                {file.path} - {file.size} bytes
-              </li>
-            ))}
-          </ul>
-        </aside>
-    </section>
-  );
+    return (
+      <Dropzone onDrop={this.onDrop}>
+        {({getRootProps, getInputProps}) => (
+          <section className="container">
+            <div {...getRootProps({className: 'dropzone'})}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            <aside>
+              <h4>Files</h4>
+              <ul>{files}</ul>
+            </aside>
+          </section>
+        )}
+      </Dropzone>
+    );
+  }
 }
 
-export default StyledDropzone;
+export default Basic;
