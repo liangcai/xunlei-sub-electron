fa3330ab51a457d88c9029dba4f7abc9812bdeb0
liang cai <cail1844@gmail.com>
2020-05-02 07:49:19 +0800

    1. 开发环境使用react服务器热重载 2. 下载功能替换为electron-dl使用ipc通信 3. moment更新带来的cannot find module './locale'异常

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,44 +1,53 @@
 import React from "react";
 import { Button } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
-import FileDownload from "js-file-download";
-import Axios from "axios";
-import qs from "qs";
+// import FileDownload from "js-file-download";
+// import Axios from "axios";
+// import qs from "qs";
 
+const ipcRenderer = window.electron.ipcRenderer
 
 function DownloadBtn(props) {
-
   const handDownloadBtnClick = (event) => {
     if (props.selectedSubs === 0 || props.selectedSubs === undefined) {
-      console.log("请先选择要下载的字幕");
       alert("请先选择要下载的字幕");
     } else {
-      props.selectedSubs.map(item => {
-      let sname = item.fpath.replace(/^.*[\\\/]/, '').replace(/^\.[^\.]*$/, '');
-      let sext = item.surl.replace(/^.*\./, '');
-      let sfullname = sname + "." + sext;
-      let url = "http://127.0.0.1:5000/api/downsub";
-      let data = {
-        surl: item.surl,
-        sname: sfullname
-      }
-      console.log("download, url:", url, "data:", qs.stringify(data));
-        Axios(
-          {
-            url:url, 
-            method: 'POST',
-            data: qs.stringify(data),
-            headers: {'content-type': 'application/x-www-form-urlencoded'}
+      props.selectedSubs.map((item) => {
+        let sname = item.fpath
+          .replace(/^.*[\\\/]/, "")
+          .replace(/^\.[^\.]*$/, "");
+        let sext = item.surl.replace(/^.*\./, "");
+        let savein = item.fpath.replace(/[^/]*?$/, "");
+        let sfullname = sname + "." + sext;
+
+        console.log("download url: ", item.surl)
+
+        ipcRenderer.send("download", {
+            url: item.surl,
+            properties: { directory: savein, filename: sfullname },
           })
-          .then((res) => {
-            console.log(res.headers);
-            let filename = decodeURI(res.headers['content-disposition'].match(/filename=(.*)/)[1]);
-            // let filename = sfullname;
-            FileDownload(res.data, filename);
-          });
-          return true;
-      })
-    };
+
+        ipcRenderer.on("download complete", (event, file) => {
+            console.log(`下载 ${file} 完成`); // Full file path
+        });
+
+        // console.log("download, url:", url, "data:", qs.stringify(data));
+        //   Axios(
+        //     {
+        //       url:url,
+        //       method: 'POST',
+        //       data: qs.stringify(data),
+        //       headers: {'content-type': 'application/x-www-form-urlencoded'}
+        //     })
+        //     .then((res) => {
+        //       console.log(res.headers);
+        //       let filename = decodeURI(res.headers['content-disposition'].match(/filename=(.*)/)[1]);
+        //       // let filename = sfullname;
+        //       FileDownload(res.data, filename);
+        //     });
+        //     return true;
+      });
+    }
   };
 
   return (
@@ -54,6 +63,5 @@ function DownloadBtn(props) {
     </Button>
   );
 }
-
 
 export default DownloadBtn;
